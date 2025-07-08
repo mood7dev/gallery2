f30f82a6650a70156d20e5190cf42713d2e46659
mood7dev <mood7dev@gmail.com>
2025-07-08 16:22:20 +0900

    첫 커밋

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <h1 class="mt-5">┃HELLO, HOME<span class="clover">🍀</span></h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  font-weight: 700;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.clover {
+  font-size: 24px;
+  line-height: 1;
+}
+</style>
